798de1e367e3377501e2f0367b4bdc780e06c849
gautamkumar1 <gautamraaz936@gmail.com>
2024-05-26 22:21:33 +0530

    Day 1

---
+++ day_01/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-blend-color-burn">
+      Hello ReactJs
+    </h1>
+    </>
+  )
+}
+
+export default App
